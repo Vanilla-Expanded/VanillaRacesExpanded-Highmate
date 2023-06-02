27b2a418fbc68fb51e7cf5dfb5aceb78a72f2515
juanosarg <31105173+juanosarg@users.noreply.github.com>
2023-06-02 12:28:25 +0200

    Initial commit

---
+++ 1.4/Source/VanillaRacesExpanded-Highmate/VanillaRacesExpanded-Highmate/DefOfs/InternalDefOf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace VanillaRacesExpandedHighmate
+{
+    [DefOf]
+    public static class InternalDefOf
+    {
+        
+
+
+
+    }
+}
