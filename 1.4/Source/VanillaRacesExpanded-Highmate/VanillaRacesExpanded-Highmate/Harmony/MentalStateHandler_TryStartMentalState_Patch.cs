db353168db260ef09798eae26e9754d2ef7a2791
Taranchuk <nfhfyxer@gmail.com>
2023-06-16 17:13:21 +0300

    bond trait effects

---
+++ 1.4/Source/VanillaRacesExpanded-Highmate/VanillaRacesExpanded-Highmate/Harmony/MentalStateHandler_TryStartMentalState_Patch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Verse;
+using Verse.AI;
+
+namespace VanillaRacesExpandedHighmate
+{
+    [HarmonyPatch(typeof(MentalStateHandler), "TryStartMentalState")]
+    public static class MentalStateHandler_TryStartMentalState_Patch
+    {
+        public static bool Prefix(Pawn ___pawn, ref bool __result, MentalStateDef stateDef)
+        {
+            if (___pawn.health.hediffSet.GetFirstHediffOfDef(InternalDefOf.VRE_PsychicBondAbrasive) != null
+                 && typeof(MentalState_InsultingSpree).IsAssignableFrom(stateDef.stateClass))
+            {
+                __result = false;
+                return false;
+            }
+            return true;
+        }
+    }
+}
