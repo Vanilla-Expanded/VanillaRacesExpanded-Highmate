4cd0a88d272788a3af8fd9129061f7392d2b05c7
Taranchuk <nfhfyxer@gmail.com>
2023-06-24 22:31:10 +0300

    log removal

--- 1.4/Source/PsychicBond/BondUtils.cs
+++ 1.4/Source/PsychicBond/BondUtils.cs
@@ -20,16 +20,11 @@ namespace VanillaRacesExpandedHighmate
                             if (pawn.MapHeld == target.MapHeld && (bondHediffTrait.traitRequirements.Any(x => x.HasTrait(pawn)
                                     || bondHediffTrait.traitRequirements.Any(x => x.HasTrait(target)))))
                             {
-                                Log.Message(pawn + " - Adding " + bondHediffTrait.hediff);
                                 bondHediffTrait.Apply(pawn);
                                 bondHediffTrait.Apply(target);
                             }
                             else
                             {
-                                if (pawn.health.hediffSet.GetFirstHediffOfDef(bondHediffTrait.hediff) != null)
-                                {
-                                    Log.Message(pawn + " - Removing " + bondHediffTrait.hediff);
-                                }
                                 bondHediffTrait.TryRemove(pawn);
                                 bondHediffTrait.TryRemove(target);
                             }
@@ -46,10 +41,6 @@ namespace VanillaRacesExpandedHighmate
                     {
                         foreach (var bondHediffTrait in def.bondHediffTraits)
                         {
-                            if (pawn.health.hediffSet.GetFirstHediffOfDef(bondHediffTrait.hediff) != null)
-                            {
-                                Log.Message(pawn + " - 2 Removing " + bondHediffTrait.hediff);
-                            }
                             bondHediffTrait.TryRemove(pawn);
                         }
                     }
