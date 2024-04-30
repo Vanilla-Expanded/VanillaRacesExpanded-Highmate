6ac5a4bf535c2421ed4cc141aa36715ed33fe2f6
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-04-30 19:23:00 +0200

    v2.01

--- 1.5/Source/Harmony/Pawn_RoyaltyTracker_OnPostTitleChanged.cs
+++ 1.5/Source/Harmony/Pawn_RoyaltyTracker_OnPostTitleChanged.cs
@@ -7,7 +7,7 @@ using Verse;
 
 namespace VanillaRacesExpandedHighmate
 {
-  
+
 
     [HarmonyPatch(typeof(Pawn_RoyaltyTracker), "OnPostTitleChanged")]
     public static class VanillaRacesExpandedHighmate_Pawn_RoyaltyTracker_OnPostTitleChanged_Patch
@@ -15,13 +15,13 @@ namespace VanillaRacesExpandedHighmate
         public static void Postfix(Pawn_RoyaltyTracker __instance, Faction faction, RoyalTitleDef prevTitle, RoyalTitleDef newTitle)
         {
             if (newTitle != null && __instance.pawn.IsColonist && PawnGenerator.IsBeingGenerated(__instance.pawn) is false
-                && Current.CreatingWorld is null && __instance.pawn.Dead is false && __instance.pawn.Map!=null
-                && (prevTitle is null || prevTitle.seniority < newTitle.seniority)
-                && (newTitle == InternalDefOf.Baron || prevTitle is null && newTitle.seniority > InternalDefOf.Baron.seniority)
+                && Current.CreatingWorld is null && __instance.pawn.Dead is false && __instance.pawn.Map != null
+                && (prevTitle is null || prevTitle.seniority < InternalDefOf.Baron.seniority)
+                && newTitle.seniority >= InternalDefOf.Baron.seniority
                 && faction == Faction.OfEmpire)
             {
 
-                var letter = LetterMaker.MakeLetter("VRE_AwardedHighmate".Translate(),"VRE_AwardedHighmateDesc".Translate(__instance.pawn.LabelCap),LetterDefOf.PositiveEvent);              
+                var letter = LetterMaker.MakeLetter("VRE_AwardedHighmate".Translate(), "VRE_AwardedHighmateDesc".Translate(__instance.pawn.LabelCap), LetterDefOf.PositiveEvent);
                 Find.LetterStack.ReceiveLetter(letter);
                 PawnGenerationRequest request = new PawnGenerationRequest(Faction.OfPlayer.def.basicMemberKind, Faction.OfPlayer, PawnGenerationContext.NonPlayer, -1, forceGenerateNewPawn: true, allowDead: false, allowDowned: false, canGeneratePawnRelations: false, false, 20f, forceAddFreeWarmLayerIfNeeded: false, allowGay: true, allowPregnant: false, allowFood: true, allowAddictions: true, inhabitant: false, certainlyBeenInCryptosleep: false, forceRedressWorldPawnIfFormerColonist: false, worldPawnFactionDoesntMatter: false, 0f, 0f, null, 1f, null, null, null, null, null, null, null, null, null, null, null, null, forceNoIdeo: false, forceNoBackstory: false, forbidAnyTitle: false, forceDead: false, null, null, InternalDefOf.Highmate, null, null, 0f, DevelopmentalStage.Adult, null, null);
                 Pawn pawn = PawnGenerator.GeneratePawn(request);
